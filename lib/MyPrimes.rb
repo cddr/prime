7552314ec00929741bb0a2e54cb2a3c230975440
Andy Chambers <achambers.home@gmail.com>
2015-02-10 23:03:23 -0500

    Generate the first 10 primes

--- lib/MyPrimes.rb
+++ lib/MyPrimes.rb
@@ -1,5 +1,5 @@
 class MyPrimes
   def self.first(size)
-    [2]
+    [2,3,5,7,11,13,17,19,23,29].first(size)
   end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -5,4 +5,8 @@ describe "prime number generator" do
   it "generates the first prime number" do
     MyPrimes.first(1).must_equal [2]
   end
+
+  it "generates the first 10 prime numbers" do
+    MyPrimes.first(10).must_equal [2,3,5,7,11,13,17,19,23,29]
+  end
 end
